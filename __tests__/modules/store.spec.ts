83c2387f0ffd93aada41f3c901353bac6ed2baca
David Valero <dvdvalero89@gmail.com>
2021-02-05 21:06:17 +0100

    Add missing store test

--- __tests__/modules/store.spec.ts
+++ __tests__/modules/store.spec.ts
@@ -6,20 +6,22 @@ import {FilesAreEquals} from "../test.util";
 const testSrcFolder = '__tests__/testSrc'
 
 beforeEach(() => {
-  if(fs.existsSync(testSrcFolder)) {
-    fs.rmdir(testSrcFolder, { recursive: true }, () => {})
-  }
+  clearSrc()
 })
 
 afterAll(() => {
+  clearSrc()
+})
+
+function clearSrc () {
   if(fs.existsSync(testSrcFolder)) {
     fs.rmdir(testSrcFolder, { recursive: true }, () => {})
   }
-})
+}
 
 describe('Store module', () => {
 
-  test('Generate files and folders', async () => {
+  test('Generate new files and folders', async () => {
     const sectionId = 'settings'
     const langId = SupportedLocale['es-ES']
     const data = {
@@ -27,6 +29,24 @@ describe('Store module', () => {
       anotherKey: 'anotherValue'
     }
 
+    await StoreLocales(testSrcFolder, sectionId, langId, data)
+
+    const outputFile = testSrcFolder + '/settings/i18n/es-ES.po'
+    const testFile = '__tests__/comparisons/store-esES.po'
+    expect(fs.existsSync(testFile)).toBe(true)
+    expect(fs.existsSync(outputFile)).toBe(true)
+    expect(FilesAreEquals(testFile, outputFile)).toBe(true)
+  })
+
+  test('Generate existing', async () => {
+    const sectionId = 'settings'
+    const langId = SupportedLocale['es-ES']
+    const data = {
+      testKey: 'testValue',
+      anotherKey: 'anotherValue'
+    }
+
+    await StoreLocales(testSrcFolder, sectionId, langId, {})
     await StoreLocales(testSrcFolder, sectionId, langId, data)
 
     const outputFile = testSrcFolder + '/settings/i18n/es-ES.po'
