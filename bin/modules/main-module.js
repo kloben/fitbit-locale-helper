1733516e0a1b72b628e607a6cae40e787898e20c
David Valero <dvdvalero89@gmail.com>
2021-02-13 14:16:39 +0100

    Final build

---
+++ bin/modules/main-module.js
@@ -0,0 +1,46 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.StartGeneration = void 0;
+var GeneratedLocales_1 = require("../classes/GeneratedLocales");
+var fitbit_locale_config_interface_1 = require("../interfaces/fitbit-locale-config.interface");
+var config_module_1 = require("./config.module");
+var dates_module_1 = require("./dates.module");
+var existing_module_1 = require("./existing.module");
+var store_module_1 = require("./store.module");
+function generateDateTimes(generated, languages, dateTimes) {
+    for (var _i = 0, languages_1 = languages; _i < languages_1.length; _i++) {
+        var langId = languages_1[_i];
+        for (var _a = 0, dateTimes_1 = dateTimes; _a < dateTimes_1.length; _a++) {
+            var cfg = dateTimes_1[_a];
+            var keys = dates_module_1.GenerateDateLocales(langId, cfg);
+            generated.store(cfg.folder, langId, keys);
+        }
+    }
+}
+function generateExistingKeys(generated, languages, localesFolder) {
+    for (var _i = 0, _a = Object.values(fitbit_locale_config_interface_1.FitbitFolder); _i < _a.length; _i++) {
+        var folder = _a[_i];
+        for (var _b = 0, languages_2 = languages; _b < languages_2.length; _b++) {
+            var langId = languages_2[_b];
+            var keys = existing_module_1.GenerateExistingLocales(localesFolder, folder, langId);
+            if (keys) {
+                generated.store(folder, langId, keys);
+            }
+        }
+    }
+}
+function StartGeneration() {
+    var config = config_module_1.GetConfig();
+    if (!config) {
+        return Promise.resolve(0);
+    }
+    var generated = new GeneratedLocales_1.GeneratedLocales();
+    if (config.dateTimes && config.dateTimes.length) {
+        generateDateTimes(generated, config.languages, config.dateTimes);
+    }
+    if (config.localesFolder) {
+        generateExistingKeys(generated, config.languages, config.localesFolder);
+    }
+    return store_module_1.StoreLocales(config.srcRootFolder, generated.locales);
+}
+exports.StartGeneration = StartGeneration;
